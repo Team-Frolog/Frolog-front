a9f09cd6410fe9abcab3b9913cb57b7fdc208858
JIMIN1020 <jimins4920@gmail.com>
2024-03-20 01:18:03 +0900

    [Style/#3]: 레이아웃 잡기

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { GlobalStyles } from './styles/GlobalStyles';
 import Home from './pages/Home';
+import MainLayout from './components/layout/MainLayout';
 
 function App() {
+  const setScreenSize = () => {
+    const vh = window.innerHeight * 0.01; // 하단 bar 제외 높이 -> 100등분 (1vh 구하기 위함)
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+
+  useEffect(() => {
+    setScreenSize();
+  });
+
   return (
     <>
       <GlobalStyles />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route element={<MainLayout />}>
+          <Route path='/' element={<Home />} />
+        </Route>
       </Routes>
     </>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Home() {
-  return <div>Home</div>;
+  return <Container>Home</Container>;
 }
 
 export default Home;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
