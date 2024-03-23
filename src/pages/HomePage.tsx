854cf8855dd9600c24d0735b7d8839b76d5118e9
Hannah1011 <hannahjoah1011@gmail.com>
2024-03-23 23:29:13 +0900

    [Feat#2]: 헤더 컴포넌트

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function Home() {
-  return <Container>Home</Container>;
+function HomePage() {
+  return <Container>HomePage</Container>;
 }
 
-export default Home;
+export default HomePage;
 
 const Container = styled.div`
   width: 100%;
---
+++ src/pages/NoticePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NoticePage() {
+  return (
+    <div>
+      {/* TODO: NoticePage 전용 스타일 및 레이아웃 */}
+      <h1>Notice Page</h1>
+      {/* TODO: NoticePage의 다른 컨텐츠들 */}
+    </div>
+  );
+}
+
+export default NoticePage;
